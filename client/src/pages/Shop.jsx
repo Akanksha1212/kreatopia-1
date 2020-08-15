4f54ffc7bd3b48fd7ac50d813f5ce3fab2837dd7
Akanksha1212 <akankshasingh121297@gmail.com>
2020-08-16 05:14:16 +0530

    Added about us

--- client/src/pages/Shop.jsx
+++ client/src/pages/Shop.jsx
@@ -10,7 +10,7 @@ import {
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import Store from "../components/store";
 import "./style.scss";
-import FooterPage from '../components/footer';
+import Footer from '../components/footer';
 
 export default (props) => {
     const [openPanel, setOpenPanel] = useState(false);
@@ -30,7 +30,7 @@ export default (props) => {
         </Toolbar>
       </AppBar>
       <Store/>
-      <FooterPage/>
+      <Footer/>
     </div>
   );
 };
